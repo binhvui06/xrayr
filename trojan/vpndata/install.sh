9d49df3a302c476b3a037513a359012cb1225be8
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-05-19 13:58:25 +0700

    Update install.sh

--- trojan/vpndata/install.sh
+++ trojan/vpndata/install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/wellcome.sh)
-bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/installsetup.sh)
 bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/1stack/main/trojan/vpndata/1stack.sh)
