217b74c01ffd4c316ef7a45b88e47cd5b49067c0
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-05-19 11:08:59 +0700

    Update install.sh

--- v2ray/vpndata/install.sh
+++ v2ray/vpndata/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo ufw allow 80/tcp && sudo ufw allow 443/tcp && sudo ufw allow 80 && sudo ufw allow 443
+bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/allowport/main/allowport.sh)
 rm -rf $0
 bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/wellcome.sh)
 sleep 5
