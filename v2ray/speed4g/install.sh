8b942ba864359115fae727451e3eb2056a1dec1c
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-05-19 13:51:23 +0700

    Update install.sh

--- v2ray/speed4g/install.sh
+++ v2ray/speed4g/install.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/wellcome.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/installsetup.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/1stack/main/v2ray/speed4g/1stack.sh)
