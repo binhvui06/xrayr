4159eb09f308169b466883cad104cf77d2d70eb8
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-05-19 13:57:44 +0700

    Update install.sh

--- v2ray/vpndata/install.sh
+++ v2ray/vpndata/install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/wellcome.sh)
-bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/installsetup.sh)
 bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/1stack/main/v2ray/vpndata/1stack.sh)
