0235dcc30f84b5d879fc6499c55afc85d7f2a874
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-05-24 18:13:09 +0700

    Delete v2ray/vpndata directory

--- v2ray/vpndata/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/wellcome.sh)
-bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/1stack/main/v2ray/vpndata/1stack.sh)
