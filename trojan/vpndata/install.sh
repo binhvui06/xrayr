560c7a07ff4595b3da16072567169efcd1d1b898
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-05-24 18:12:50 +0700

    Delete trojan directory

--- trojan/vpndata/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/wellcome.sh)
-bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/1stack/main/trojan/vpndata/1stack.sh)
